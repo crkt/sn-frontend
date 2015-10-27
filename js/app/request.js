74f9fa56ae5d1f808220c54467a3bc61e9a3db8d
phcr <philipmayoo@gmail.com>
2015-10-27 09:56:53 +0000

    created the request module, currently has a post method

--- js/app/request.js
+++ js/app/request.js
@@ -1,59 +1,50 @@
 define([], function() {
+  
+  // The javascript object to return to requirejs.
+  // e.g what will be exported to other modules.
+  var exports = {};
 
-    var exports = {};
-
-    var registerRequest = function(data, success, failure) {
-	
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "/stores", true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	xhr.setRequestHeader("Accept", "application/json");
-
-	xhr.ontimeout = function () {
-	    failure();
-	};
-	
-	xhr.onload = function () {
-	    if (xhr.readyState === 4) {
-		if (xhr.status === 200) {
-		    success(xhr, xhr.response);
-		} else {
-		    failure(xhr, xhr.response);
-		}
-	    }
-	};
-
-	xhr.onerror = function (e) {
-	    console.log("FAILED TO SUBMIT");
-	    failure(xhr.statusText);
-	}
-	
-
-	xhr.send(JSON.stringify(data));
+  /**
+     Sends a POST request to with data and to the destination specified.
+     Only sends with JSON.
+     From https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+   **/
+  var postRequest = function(data, dest, success, failure) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", dest, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Accept", "application/json");
+    
+    // If the request timeouts.
+    xhr.ontimeout = function () {
+      failure();
     };
-
-    var searchMovies = function (success,failure) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "/movies", true);
-	xhr.setRequestHeader("Content-Type", "application/json");
-	xhr.setRequestHeader("Accept", "application/json");
-
-	xhr.onload = function () {
-	    if (xhr.readyState === 4) {
-		if (xhr.status === 200) {
-		    success(xhr,JSON.parse(xhr.response));
-		} else {
-		    failure(xhr, xhr.response);
-		}
-	    }
+    
+    // If things go as planned, run the success function that was supplied.
+    // if not, the failure function.
+    xhr.onload = function () {
+      if (xhr.readyState === 4) {
+	if (xhr.status === 200) {
+	  success(xhr, xhr.response);
+	} else {
+	  failure(xhr, xhr.response);
 	}
-
-	xhr.send();
+      }
+    };
+    
+    // If shit goes wrong, we want to know and why.
+    xhr.onerror = function (e) {
+      console.log("Post request failed.");
+      failure(xhr.statusText);
     }
+    
+    // Sends the data as a JSON object.
+    xhr.send(JSON.stringify(data));
+  };
 
-
-    exports.register = registerRequest;
-    exports.index = storesIndex;
-
-    return exports;
+  
+  // The functions to export.
+  exports.post = postRequest;
+  
+  return exports;
 });
