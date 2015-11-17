4b544bd0643109251fea6da007818a08bf7a5b77
phcr <philipmayoo@gmail.com>
2015-11-17 21:04:52 +0000

    created on click methods

--- js/views/user/login.js
+++ js/views/user/login.js
@@ -4,14 +4,14 @@ define(['models/user',
 {
   var exports = {};
 
-  function Register(callback) {
+  function Register(callback, cancel) {
     Base.Base.call(this, "div");
-    this.addClass("active");
     this.setAttribute("id", "register");
     
     this.callback = callback;
+    this.cancel = cancel;
 
-    this.form = new FormWidget.Form("user-form", this.onSubmit.bind(this));
+    this.form = new FormWidget.Form(this.onSubmit.bind(this));
     this.form.addClass("user-form");
 
     this.model = new User();
@@ -31,16 +31,25 @@ define(['models/user',
                                                    "Re-enter password", 
                                                    this.onChange.bind(this, "repassword"));
     this.submitButton = new Base.Button("submit", "Register");
+    this.submitButton.setDisabled(true);
+
+    this.cancelButton = new Base.Button("button", "Cancel", 
+                                        this.onCancelClick.bind(this));
 
     this.form.addChild(this.email.element);
     this.form.addChild(this.password.element);
     this.form.addChild(this.repassword.element);
     this.form.addChild(this.submitButton.element);    
+    this.form.addChild(this.cancelButton.element);
     this.addChild(this.form.element);
   }
 
   Register.prototype = Object.create(Base.Base.prototype);
   Register.prototype.constructor = Register;
+
+  Register.prototype.onCancelClick = function (e) {
+    this.cancel();
+  }
 
   Register.prototype.isValid = function () {
     return (this.model.isValid() &&
@@ -71,6 +80,14 @@ define(['models/user',
     this.callback(this.model, e);
   }
 
+  Register.prototype.toggleActive = function (t) {
+    this.toggleClass("active", t);
+  }
+
+  Register.prototype.toggleVisible = function (t) {
+    this.toggleClass("active", t);
+  }
+
   Register.prototype.reset = function () {
     this.model = new User();
     this.email.input.clear();
@@ -79,20 +96,107 @@ define(['models/user',
     this.submitButton.setDisabled(true);
   }
 
-  function Login() {
+  function Login(callback, register) {
     Base.Base.call(this, "div");
     this.setAttribute("id", "login");
 
-    this.form = new FormWidget.Form();
+    this.register = register;
+    this.callback = callback;
+
+    this.form = new FormWidget.Form(this.onSubmit.bind(this));
     this.form.addClass("user-form");
 
+    this.model = new User();
+
+    this.email = new FormWidget.CompoundInput("text", 
+                                              "Email", 
+                                              "Your email",
+                                              this.onChange.bind(this, "email"));
+
+    this.password = new FormWidget.CompoundInput("password", 
+                                                 "Password", 
+                                                 "Enter password", 
+                                                 this.onChange.bind(this, "password"));
+
+    this.submitButton = new Base.Button("submit", "Login");
+    this.submitButton.setDisabled(true);
+
+    this.registerButton = new Base.Button("button", "Create user", this.onRegisterClick.bind(this));
+    this.registerButton.setDisabled(false);
+
+    this.form.addChild(this.email.element);
+    this.form.addChild(this.password.element);
+    this.form.addChild(this.submitButton.element);    
+    this.form.addChild(this.registerButton.element);
     this.addChild(this.form.element);
   }
 
   Login.prototype = Object.create(Base.Base.prototype);
   Login.prototype.constructor = Login;
 
+  Login.prototype.onRegisterClick = function (e) {
+    console.log("Create user clicked");
+    this.register();
+  }
 
+  Login.prototype.toggleActive = function (t) {
+    this.toggleClass("active", t);
+  }
+  
+  Login.prototype.toggleVisible = function (t) {
+    this.toggleClass("active", t);    
+  }
+
+  Login.prototype.isValid = function () {
+    return (this.model.isValid());
+  }
+
+  Login.prototype.onChange = function (prop, value) {
+    if (prop == "email") {
+      this.model.setEmail(value);
+    } else if (prop == "password") {
+      this.model.setPassword(value);
+    }
+    this.submitButton.setDisabled(!this.isValid());
+  }
+
+  Login.prototype.onSubmit = function (e) {
+    console.log("Login");
+    this.callback(this.model, e);
+  }
+
+
+  function Profile() {
+    Base.Base.call(this, "div");
+    this.setAttribute("id", "user-profile");
+
+    this.infoPanel = new Base.Base("div");
+    this.info = new Base.Label("Logged in as: ");
+    this.infoEmail = new Base.Label();
+
+    this.infoPanel.addChild(this.info.element);
+    this.infoPanel.addChild(this.infoEmail.element);
+
+    this.addChild(this.infoPanel.element);
+  }
+
+  Profile.prototype = Object.create(Base.Base.prototype);
+  Profile.prototype.constructor = Profile;
+
+  Profile.prototype.toggleActive = function (t) {
+    this.toggleClass("active", t);
+  }
+
+  Profile.prototype.toggleVisible = function (t) {
+    this.toggleClass("active", t);
+  }
+
+  Profile.prototype.setUser = function (user) {
+    this.user = user;
+    this.infoEmail.setContent(user.email);
+  }
+
+  exports.Profile = Profile;
   exports.Register = Register;
   exports.Login = Login;
   
