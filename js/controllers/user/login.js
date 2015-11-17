5c7ab4c6e3bb3ac9b5d8f47995dd6da2ec0ec1cb
phcr <philipmayoo@gmail.com>
2015-11-17 21:04:38 +0000

    added on click methods

--- js/controllers/user/login.js
+++ js/controllers/user/login.js
@@ -1,24 +1,52 @@
 define(['views/user/login', 
         'app/request', 
-        'models/user'], function(UserView, Request, UserModel) {
+        'models/user'], function(UserView, Request, UserModel) 
+{
   
   function User(onLogin) {
 
     this.view = document.querySelector("#user");  
   
-    this.login = new UserView.Login();
-    this.register= new UserView.Register(this.submit.bind(this, "register"));
+    this.login = new UserView.Login(this.submit.bind(this, "login"),
+                                   this.toggleRegisterLogin.bind(this));
+    this.login.toggleVisible(true);
+
+    this.register= new UserView.Register(this.submit.bind(this, "register"),
+                                        this.toggleRegisterLogin.bind(this));
+    this.register.toggleVisible(false);
+
+    this.profile = new UserView.Profile();
+    this.profile.toggleVisible(false);
 
     this.view.appendChild(this.login.element);
     this.view.appendChild(this.register.element);
+    this.view.appendChild(this.profile.element);
 
     this.onLogin = onLogin
+  }
+
+  User.prototype.toggleRegisterLogin = function () {
+    var active = this.view.querySelector(".active");
+    if (active === this.login.element) {
+      this.login.toggleActive(false);
+      this.register.toggleActive(true);
+    } else if (active == this.register.element) {
+      this.register.toggleActive(false);
+      this.login.toggleActive(true);
+    }
+                  
+  }
+
+  User.prototype.userLoggedIn = function (user) {  
+    this.register.toggleVisible(false);
+    this.profile.setUser(user);
+    this.profile.toggleVisible(true);
   }
 
   User.prototype.success = function (xhr, response) {
     // There be dragons here.
     this.onLogin(new UserModel(response.email, response.id));
-    this.register.clear();
+    this.register.reset();
   }
 
   User.prototype.failure = function (xhr, response) {
