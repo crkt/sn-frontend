ff1ca5eeca85063fca3ee34b4b4bbd28e17363aa
phcr <philipmayoo@gmail.com>
2015-11-17 15:08:15 +0000

    created base widget files

---
+++ js/views/widgets/base.js
@@ -0,0 +1,83 @@
+define([], function() {
+
+  var widgets = {};
+
+  function Base(element) {
+    this.element = document.createElement(element);
+  }
+
+  Base.prototype.setEvent = function(event, f) {
+    this.element[event] = f;
+  }
+
+  Base.prototype.setAttribute = function (x, y) {
+    this.element.setAttribute(x,y);
+  }
+
+  Base.prototype.addChild = function (x) {
+    this.element.appendChild(x);
+  }
+
+  Base.prototype.addClass = function (x) {
+    this.element.classList.add(x);
+  }
+
+  
+  function Input(type, placeholder, callback) {
+    Base.call(this, "input");
+    this.element.type = type;
+    this.element.placeholder = placeholder;
+    this.callback = callback;
+
+    this.setEvent("onchange", this.onChange.bind(this));
+  }
+
+  Input.prototype = Object.create(Base.prototype);
+  Input.prototype.constructor = Input;
+
+  Input.prototype.onChange = function (e) {
+    e.preventDefault();
+    console.log("Input base onChange");
+    this.callback(e.target.value);
+  }
+
+  Input.prototype.clear = function () {
+    this.element.value = "";
+  }
+
+  Input.prototype.getValue = function () {
+    return this.element.value;
+  }
+  
+
+
+  function Label(text) {
+    Base.call(this, "label");
+    this.element.textContent = text;
+  }
+
+  Label.prototype = Object.create(Base.prototype);
+  Label.prototype.constructor = Label;
+
+  function Button(type, text) {
+    Base.call(this, "button");
+    this.element.type = type;
+    this.element.textContent = text;
+  }
+
+  Button.prototype = Object.create(Base.prototype);
+  Button.prototype.constructor = Button;
+
+  Button.prototype.setDisabled = function (t) {
+    this.element.disabled = t;
+  }
+
+  widgets.Base = Base;
+  widgets.Input = Input;
+  widgets.Label = Label;
+  widgets.Button = Button;
+
+  return widgets;
+
+});
+
---
+++ js/views/widgets/form.js
@@ -0,0 +1,52 @@
+define(['views/widgets/base'], function(Base) {
+  
+  var exports = {};
+
+  function Form(onsubmit) {
+    Base.Base.call(this, "form");
+    
+    if (onsubmit) {
+      this.setEvent("onsubmit", onsubmit);
+    } else {
+      this.setEvent("onsubmit", this.onSubmit.bind(this));
+    }
+  }
+
+  Form.prototype = Object.create(Base.Base.prototype);
+  Form.prototype.constructor = Form;
+
+  Form.prototype.onSubmit = function (e) {
+    e.preventDefault();
+    console.log("form submit yo");
+  };
+
+  function CompoundInput(type, placeholder, label, callback) {
+    Base.Base.call(this, "div");
+    this.addClass("form-input");
+    
+    this.callback = callback;
+    
+    this.label = new Base.Label(label);
+    this.label.addClass("form-input-label");
+
+    this.input = new Base.Input(type, placeholder, this.onChange.bind(this));
+    this.input.addClass("form-input-input");
+
+    this.addChild(this.label.element);
+    this.addChild(this.input.element);
+  }
+
+  CompoundInput.prototype = Object.create(Base.Base.prototype);
+  CompoundInput.prototype.constructor = CompoundInput;
+
+  CompoundInput.prototype.onChange = function (value) {
+    console.log("Reg-widget on change");
+    this.callback(value);
+  }
+
+  exports.CompoundInput = CompoundInput;
+  exports.Form = Form;
+
+  return exports;
+
+});
