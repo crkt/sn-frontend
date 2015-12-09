bd81c6f02d1d35247cbd1fe70bc0e4b6f38dc608
phcr <philipmayoo@gmail.com>
2015-12-09 18:29:08 +0000

    Added query parser to utils

--- js/movie.js
+++ js/movie.js
@@ -1,10 +1,6 @@
-define(['movie/detail','api'], function(Detail, API) {
+define(['movie/detail','api', 'utils'], function(Detail, API, Utils) {
   
-  function getQueryStringValue (key) {  
-    return unescape(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + escape(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));  
-  }
-
-  var movie_id = parseInt(getQueryStringValue("id"));
+  var movie_id = parseInt(Utils.getQueryStringValue("id"));
 
   var detail = new Detail();
   document.querySelector("#moviez-detail").appendChild(detail.view.dom);
