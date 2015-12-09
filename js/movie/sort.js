37aad014892d7c68b44df991482f569404890b27
msavikko <savikko@gmail.com>
2015-12-09 12:05:43 +0100

    added comparefunctions

--- js/movie/sort.js
+++ js/movie/sort.js
@@ -7,12 +7,61 @@ define([], function() {
     this.title = this.dom.querySelector(".title");
   }  
 
-
   function Sort(view) {
     this.view = view || new SortView();
   }
 
+  Sort.prototype.sortByTitle = function (lst, ascending) {
+    var sort = undefined;
+    if (ascending) {
+      sort = function (a,b) {
+        if (a > b) {
+          return 1;
+        } else if (a < b) {
+          return -1;
+        }
+        return 0;
+      }
+    } else {
+      sort = function (a,b) {
+        if (a > b) {
+          return -1;
+        } else if (a < b) {
+          return 1;
+        }
+        return 0;
+      }
+    }
+    return lst.sort(sort);
+  }
+
+  Sort.prototype.sortByRating = function (lst, ascending) {
+    var sort = undefined;
+    if (ascending) {
+      sort = function (a,b) {
+        if (a.rating.rating > b.rating.rating) {
+          return 1;
+        } else if (a.rating.rating < b.rating.rating) {
+          return -1;
+        }
+        return 0;
+      }
+    } else {
+      sort = function (a,b) {
+        if (a.rating.rating > b.rating.rating) {
+          return -1;
+        } else if (a.rating.rating < b.rating.rating) {
+          return 1;
+        }
+        return 0;
+      }
+    }
+    return lst.sort(sort);
+  }
+
+
 
   return Sort;
+
 
 });
